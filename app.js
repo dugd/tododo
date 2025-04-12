51e373fad4db57846aa382f26b2385f47f9582d3
Dugd_Nitkin <dugd.nitkin@gmail.com>
2025-04-12 17:14:46 +0300

    ejs usage

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
     console.log("Here");
-    res.send("Welcome");
+    res.render('index', {text: 'Dugd'});
 });
 
 app.listen(port, () => {

2ac9e86dcdb26d6f250b93f70449db8a821b101a
Dugd_Nitkin <dugd.nitkin@gmail.com>
2025-04-12 18:05:31 +0300

    examples of routes usage

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 
-const app = express();
+const taskRouter = require('./routes/tasks');
+
 const port = process.env.PORT || 8080;
+const app = express();
 
 app.set('view engine', 'ejs');
 
@@ -9,6 +11,8 @@ app.get('/', (req, res) => {
     console.log("Here");
     res.render('index', {text: 'Dugd'});
 });
+
+app.use("/tasks", taskRouter);
 
 app.listen(port, () => {
     console.log(`server is running on port ${port}`);

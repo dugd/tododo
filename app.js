168c3c85b28a0a06068987841dcb477f3232a727
Dugd_Nitkin <dugd.nitkin@gmail.com>
2025-04-12 19:11:49 +0300

    examples of middlewares usage

--- app.js
+++ app.js
@@ -7,12 +7,24 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
+function logger(req, res, next) {
+    console.log(req.url);
+    next();
+}
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(logger);
+app.use("/tasks", taskRouter);
+
 app.get('/', (req, res) => {
-    console.log("Here");
     res.render('index', {text: 'Dugd'});
 });
 
-app.use("/tasks", taskRouter);
+app.post('/echo', (req, res) => {
+    res.send(req.body);
+})
 
 app.listen(port, () => {
     console.log(`server is running on port ${port}`);

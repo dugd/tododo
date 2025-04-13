f4bfa62196935f7cb3cf81232a26bfcc1b3e67d0
Dugd_Nitkin <dugd.nitkin@gmail.com>
2025-04-13 03:51:59 +0300

    apply mongoose db connect

--- app.js
+++ app.js
@@ -1,21 +1,30 @@
 const express = require('express');
 
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
+const dbConnect = require('./db')
+
 const taskRouter = require('./routes/tasks');
 
 const port = process.env.PORT || 8080;
 const app = express();
 
 app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
 
 function logger(req, res, next) {
     console.log(req.url);
     next();
 }
 
-app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(express.static('public'));
 app.use(logger);
+
 app.use("/tasks", taskRouter);
 
 app.get('/', (req, res) => {
@@ -26,6 +35,12 @@ app.post('/echo', (req, res) => {
     res.send(req.body);
 })
 
-app.listen(port, () => {
-    console.log(`server is running on port ${port}`);
-})
+// async start of application
+const init = async () => {
+    await dbConnect();
+    app.listen(port, () => {
+        console.log(`server is running on port ${port}`);
+    })
+}
+
+_ = init()
---
+++ db.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+const {ServerApiVersion} = require("mongodb");
+
+const url = process.env.DATABASE_URL || 'mongodb://localhost:27017';
+
+
+const dbConnect = async () => {
+    try {
+        await mongoose.connect(url, {
+            serverApi: {
+                version: ServerApiVersion.v1,
+                strict: true,
+                deprecationErrors: true,
+            }
+        });
+        console.log('MongoDB Connected');
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+};
+
+module.exports = dbConnect;

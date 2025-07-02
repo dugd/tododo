ab3e852c3c9799cea7115f060385e4ca629f3530
Dugd_Nitkin <dugd.nitkin@gmail.com>
2025-07-02 16:59:01 +0300

    remove users router

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -3,13 +3,11 @@ const express = require('express');
 const { apiErrorHandler } = require('../../middlewares/error');
 
 const taskRouter = require('./tasks');
-const userRouter = require('./users');
 const authRouter = require('./auth');
 
 const apiRouter = express.Router();
 
 apiRouter.use('/tasks', taskRouter);
-apiRouter.use('/users', userRouter);
 apiRouter.use('/auth', authRouter);
 
 apiRouter.use(apiErrorHandler);
